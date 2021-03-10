f9fef7d8423019a3a6e8eb496ac339ac1841dda6
Daniel Pérez Chan <danielpc@mesoamericana.edu.gt>
2021-03-09 21:25:45 -0600

    Se creo otra clase que nos servira para el programa

---
+++ Laboratorio_3_de_repaso/Dueño.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_3_de_repaso
+{
+    class Dueño
+    {
+        string nombre;
+        string apellido;
+        string no_casa;
+        float cuota_mantenimiento;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string No_casa { get => no_casa; set => no_casa = value; }
+        public float Cuota_mantenimiento { get => cuota_mantenimiento; set => cuota_mantenimiento = value; }
+    }
+}
