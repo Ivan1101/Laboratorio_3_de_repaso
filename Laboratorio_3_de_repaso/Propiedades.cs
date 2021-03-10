cb6f781d61fbd8b7bf3466a4cce4f4e071724082
Daniel Pérez Chan <danielpc@mesoamericana.edu.gt>
2021-03-09 21:15:55 -0600

    Se crearon las dos primeras clase que se utlizaran en el programa

---
+++ Laboratorio_3_de_repaso/Propiedades.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_3_de_repaso
+{
+    class Propiedades
+    {
+        string no_casa;
+        string dpi_dueño;
+        float cuota_mantenimiento;
+
+        public string No_casa { get => no_casa; set => no_casa = value; }
+        public string Dpi_dueño { get => dpi_dueño; set => dpi_dueño = value; }
+        public float Cuota_mantenimiento { get => cuota_mantenimiento; set => cuota_mantenimiento = value; }
+    }
+}
